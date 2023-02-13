cb5743859e1595f9a1b237fa388fe15d17d2df06
Victor Bejas <victorbejas97@gmail.com>
2023-02-12 21:28:15 -0300

    refactor component

--- islands/InputText.tsx
+++ islands/InputText.tsx
@@ -1,41 +1,37 @@
 import { useState } from "preact/hooks";
 import useGenerator from "../hooks/useGenerator.ts";
-import ResultIntent from "../components/ResultIntent.tsx";
 
 export default function InputText() {
   const [text, setText] = useState("");
   const [url, setUrl] = useState("");
 
-  const { result, counterCharacter } = useGenerator({ text, url });
+  const { counterCharacter } = useGenerator({ text, url });
 
   return (
-    <>
-      <section class="flex flex-col items-center justify-center w-full">
-        <div class="flex flex-col justify-center mt-8 w-3/4">
-          <label class="text-xl font-bold text-white">Tweet text</label>
-          <textarea
-            class="bg-transparent w-full p-2 mt-2 text-white bg-white rounded-md resize-none border border-gray-400 hover:border-gray-300"
-            placeholder="Enter your tweet text here"
-            rows={4}
-            maxLength={280}
-            value={text}
-            onInput={(e) => setText((e.target as HTMLInputElement).value)}
-          />
-          <small class="text-white italic">
-            Character remaining: {counterCharacter}
-          </small>
+    <section class="flex flex-col items-center justify-center w-full">
+      <div class="flex flex-col justify-center mt-6 w-3/4">
+        <label class="text-xl font-bold text-white">Tweet text</label>
+        <textarea
+          class="bg-transparent w-full p-2 mt-2 text-white bg-white rounded-md resize-none border border-gray-400 hover:border-gray-300"
+          placeholder="Enter your tweet text here"
+          rows={4}
+          maxLength={280}
+          value={text}
+          onInput={(e) => setText((e.target as HTMLInputElement).value)}
+        />
+        <small class="text-white italic">
+          Character remaining: {counterCharacter}
+        </small>
 
-          <label class="text-xl font-bold text-white mt-4">Url</label>
-          <input
-            class="bg-transparent w-full p-2 mt-2 text-white rounded-md border border-gray-400 hover:border-gray-300"
-            placeholder="Enter your url here"
-            type="text"
-            value={url}
-            onInput={(e) => setUrl((e.target as HTMLInputElement).value)}
-          />
-        </div>
-      </section>
-      <ResultIntent result={result} />
-    </>
+        <label class="text-xl font-bold text-white mt-2">Url</label>
+        <input
+          class="bg-transparent w-full p-2 mt-2 text-white rounded-md border border-gray-400 hover:border-gray-300"
+          placeholder="Enter your url here"
+          type="text"
+          value={url}
+          onInput={(e) => setUrl((e.target as HTMLInputElement).value)}
+        />
+      </div>
+    </section>
   );
 }
--- islands/ShareButtons.tsx
+++ islands/ShareButtons.tsx
@@ -1,21 +1,24 @@
 import { Button } from "../components/Button.tsx";
+import { resultURL } from "../signals/resultURL.ts";
 import CopyIcon from "../components/Icons/CopyIcon.tsx";
 import TwitterIcon from "../components/Icons/TwitterIcon.tsx";
 
-export default function ShareButton({ result }: { result: string }) {
+export default function ShareButton() {
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(result);
+    navigator.clipboard.writeText(resultURL.value);
   };
 
   return (
     <div class="flex items-center justify-center gap-4">
       <Button onClick={copyToClipboard}>
-        <CopyIcon />
+        <CopyIcon width={24} height={24} />
         Copy to clipboard
       </Button>
       <a
         class="bg-white border border-white inline-flex items-center justify-center gap-2 px-4 py-2 rounded-md font-medium hover:bg-black hover:text-white text-black transition-all ease-in duration-75"
-        href={result}
+        href={resultURL.value}
+        target="_blank"
+        rel="nofollow noreferrer"
       >
         <TwitterIcon width={24} height={24} />
         Share on twitter
